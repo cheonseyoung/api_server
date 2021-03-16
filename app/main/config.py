1e813aa15918c010bfadcc43a95c30d41eb874dd
cheonseyoung <pocu1407@gmail.com>
2021-03-16 22:51:53 +0900

    (5)user operations까지 진행함

--- app/main/config.py
+++ app/main/config.py
@@ -7,36 +7,36 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'ngle_api_tongchun')
-    DEBUG = False
+	SECRET_KEY = os.getenv('SECRET_KEY', 'ngle_api_tongchun')
+	DEBUG = False
 
 
 class DevelopmentConfig(Config):
-    # uncomment the line below to use postgres
-    # SQLALCHEMY_DATABASE_URI = postgres_local_base
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'flask_boilerplate_main.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+	# uncomment the line below to use postgres
+	# SQLALCHEMY_DATABASE_URI = postgres_local_base
+	DEBUG = True
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'flask_boilerplate_main.db')
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class TestingConfig(Config):
-    DEBUG = True
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'flask_boilerplate_test.db')
-    PRESERVE_CONTEXT_ON_EXCEPTION = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+	DEBUG = True
+	TESTING = True
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'flask_boilerplate_test.db')
+	PRESERVE_CONTEXT_ON_EXCEPTION = False
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class ProductionConfig(Config):
-    DEBUG = False
-# uncomment the line below to use postgres
-# SQLALCHEMY_DATABASE_URI = postgres_local_base
+	DEBUG = False
+	# uncomment the line below to use postgres
+	# SQLALCHEMY_DATABASE_URI = postgres_local_base
 
 
 config_by_name = dict(
-    dev=DevelopmentConfig,
-    test=TestingConfig,
-    prod=ProductionConfig
+	dev=DevelopmentConfig,
+	test=TestingConfig,
+	prod=ProductionConfig
 )
 
 key = Config.SECRET_KEY
